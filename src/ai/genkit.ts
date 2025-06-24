6e4b8f4d4f086f1d5eab662640117ffb390eb362
kunal Sonpitre <kunalsonpitre100@gmail.com>
2025-06-24 11:02:35 +0000

    Tum India region rakho lekin image generator service on Karo please

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,6 +3,6 @@ import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
   plugins: [
-    googleAI({apiService: 'vertex', location: 'us-east4'}),
+    googleAI({apiService: 'vertex', location: 'asia-southeast1'}),
   ],
 });
