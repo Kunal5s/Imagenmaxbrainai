91e54917328bc3967f6a0412b38b65ed965de5f3
kunal Sonpitre <kunalsonpitre100@gmail.com>
2025-06-24 09:11:09 +0000

    Not solved image generator tools

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,6 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI({apiProvider: 'vertex'})],
+  plugins: [googleAI({apiProvider: 'vertex', location: 'us-central1'})],
   model: 'googleai/gemini-2.0-flash',
 });
