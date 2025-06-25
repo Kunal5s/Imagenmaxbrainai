6d4c1a0de09041e448f849a419ab4dff00ed4882
kunal Sonpitre <kunalsonpitre100@gmail.com>
2025-06-25 11:00:59 +0000

    Firebase app hosting se kaise jodu

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -16,7 +16,7 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
+
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
 export { app };
