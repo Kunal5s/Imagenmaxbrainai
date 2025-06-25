ea678b6e9f310d959a9b9c586192b9de06061485
kunal Sonpitre <kunalsonpitre100@gmail.com>
2025-06-25 04:02:14 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,30 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApp, getApps } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCxbMsK90sgOQp1G4fICfH1v1a6-bZV-Hg",
-  authDomain: "pixel-mart-454406.firebaseapp.com",
-  projectId: "pixel-mart-454406",
-  storageBucket: "pixel-mart-454406.firebasestorage.app",
-  messagingSenderId: "349994441662",
-  appId: "1:349994441662:web:5bad52a827b29a02b6f7ef",
-  measurementId: "G-L359DEQQ4J"
+  apiKey: "AIzaSyDFDjcsIp_73LmYGMRvC31fNWP_nX0o238",
+  authDomain: "visionforge-ai-2fzsc.firebaseapp.com",
+  projectId: "visionforge-ai-2fzsc",
+  storageBucket: "visionforge-ai-2fzsc.firebasestorage.app",
+  messagingSenderId: "873860631451",
+  appId: "1:873860631451:web:eb18891174bfef3c07af3e"
 };
 
 // Initialize Firebase
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
-let analytics;
 
-// Ensure analytics is only initialized on the client side where window is available
-if (typeof window !== 'undefined') {
-  if (firebaseConfig.measurementId) {
-    analytics = getAnalytics(app);
-  }
-}
-
-export { app, analytics };
+export { app };
